386547d85d1d18a4cf2bd5f9670e49564641122d
Ivam <ivam.developer@gmail.com>
2019-07-24 11:46:00 -0300

    Settings_01

--- api/application/products/products.php
+++ api/application/products/products.php
@@ -1,3 +1,14 @@
 <?php 
-
+    // Global Variable connection
+    $connection = getConnection();
+    $data = array();
+    
+    // Consult
+    $select = "select * from products";
+    $ret = $connection ->query($select);
+    $products = $ret->fetchAll();
+    var_dump($products);
+    var_dump($products[0]['id_product']);
+    exit;
+    return $data;
 ?>
--- api/routing-modules.php
+++ api/routing-modules.php
@@ -1,3 +1,23 @@
 <?php 
+    // Includes
+    include(db.php);
+    include(products.php);
 
+    // Routes
+    function getRoutes() {
+        // Local Variables
+        $url = $_SERVER['REQUEST_URI'];
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if($method === "GET") {
+            switch($url) {
+                case "/profile":
+                    include("profile/profile.php");
+                    break;
+            }
+        }
+        if($method === "POST") {
+
+        }
+    }
 ?>
